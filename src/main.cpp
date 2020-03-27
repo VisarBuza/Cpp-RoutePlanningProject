b1f8e80bfc48aa44c39eabfcbc0bf8c67a7ad96c
Visar <visar.buza7@gmail.com>
2020-03-27 11:44:56 +0100

    Adding user input for starting coordinates

--- src/main.cpp
+++ src/main.cpp
@@ -52,15 +52,20 @@ int main(int argc, const char **argv)
             osm_data = std::move(*data);
     }
     
-    // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
-    // user input for these values using std::cin. Pass the user input to the
-    // RoutePlanner object below in place of 10, 10, 90, 90.
+    
+    float start_x, start_y, end_x, end_y;
 
+    std::cout << "\nThe starting x coordinate: "; std::cin >> start_x;
+    std::cout << "The starting y coordinate: ";   std::cin >> start_y;
+    std::cout << "The ending x coordinate: ";     std::cin >> end_x;
+    std::cout << "The ending y coordinate: ";     std::cin >> end_y;
+    std::cout << std::endl;
+    
     // Build Model.
     RouteModel model{osm_data};
 
     // Create RoutePlanner object and perform A* search.
-    RoutePlanner route_planner{model, 10, 10, 90, 90};
+    RoutePlanner route_planner{model, start_x, start_y, end_x, end_y};
     route_planner.AStarSearch();
 
     std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
